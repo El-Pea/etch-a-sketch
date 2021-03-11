70f373479a8d111b1cb0465d1d167ca10d861bf3
El-Pea <62609661+El-Pea@users.noreply.github.com>
2021-03-11 17:32:23 -0500

    setup

---
+++ app.js
@@ -0,0 +1,10 @@
+function makeGrid(number){
+    const gridTarget = document.getElementById('grid-target');
+    number = 16;
+    // this is just for testing, users will put a number in here betwee 1-64
+    for (let i; i <= number; i++){
+        
+    }
+}
+
+makeGrid();
