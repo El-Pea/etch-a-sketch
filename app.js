8630c0db3ef33297a5b8c194ede6b220118b4f16
El-Pea <62609661+El-Pea@users.noreply.github.com>
2021-03-13 17:50:33 -0500

    makeGrid() working

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 function makeGrid(number){
     number = 16; //testing
+    const gridlets = number * number;
     const gridTarget = document.getElementById('grid-target');
     const fragment = document.createDocumentFragment();
-    for (let i = 0; i < number; i++){
+    for (let i = 0; i < gridlets; i++){
         const gridlet = document.createElement('div');
+        gridlet.style.height = `${560/number}px`;
+        gridlet.style.width = `${560/number}px`;
         gridlet.classList.add('gridlet');
         fragment.appendChild(gridlet);
+
     }
     gridTarget.appendChild(fragment);
+}
+
+function paintItBlack(){
+    const gridlets = document.querySelectorAll('gridlet');
+    
+    });
+    
 }
 
 makeGrid();
